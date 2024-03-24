25960d8c0ef1297393649233002d36bb7df8da0f
Firdavs Hoshimov <firdavs.the.best@gmail.com>
2024-03-24 15:26:51 +0500

    create a settingsFragment

--- app/src/main/java/com/firdavs/android/chatroom/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/firdavs/android/chatroom/Fragments/SettingsFragment.kt
@@ -1,60 +1,86 @@
 package com.firdavs.android.chatroom.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.firdavs.android.chatroom.ModelClasses.Users
 import com.firdavs.android.chatroom.R
+import com.firdavs.android.chatroom.databinding.FragmentSearchBinding
+import com.firdavs.android.chatroom.databinding.FragmentSettingsBinding
+import com.firdavs.android.chatroom.databinding.UserSearchItemLayoutBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    var userReference: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+        // Inflate the layout for this fragment
+        _binding = FragmentSettingsBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        userReference = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+
+        userReference!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()){
+                    val user: Users? = p0.getValue(Users::class.java)
+                    val profileImageUrl = user?.getProfile()
+                    val coverImageUrl = user?.getСover()
+
+                        if (context != null){
+                            binding.usernameSettings.text = user!!.getUserName()
+                            if (!profileImageUrl.isNullOrEmpty()) {
+                                Picasso.get().load(profileImageUrl).into(binding.profileImageSettings)
+                            }
+
+                            if (!coverImageUrl.isNullOrEmpty()) {
+                                Picasso.get().load(coverImageUrl).into(binding.coverImageSettings)
+                            }
+                        }
+
+
                 }
             }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+        })
+
+        return view
     }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
